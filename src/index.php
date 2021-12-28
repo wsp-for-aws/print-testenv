1a5bb8e6b7ed6f32ade4e148d10cc9ff74469fe2
Aleksandr Prikhozhdenko <aprikhozhdenko@plesk.com>
2021-12-28 17:27:00 +0700

    Print 'FIRST' and 'SECOND' env variables

--- src/index.php
+++ src/index.php
@@ -1,5 +1,8 @@
 <?php
 
-if (isset($_ENV['TESTVAR'])) {
-    echo $_ENV['TESTVAR'];
+if (isset($_ENV['FIRST'])) {
+    echo 'FIRST: ' . $_ENV['FIRST'] . ';';
+}
+if (isset($_ENV['SECOND'])) {
+    echo 'SECOND: ' . $_ENV['SECOND'];
 }
